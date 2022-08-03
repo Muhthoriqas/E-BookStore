672b64b9ae3337cdf2d5d6a4223c1bac85bb8e3b
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2022-08-03 17:06:24 +0700

    Update Reviews Section

--- js/script.js
+++ js/script.js
@@ -164,3 +164,25 @@ var swiper = new Swiper(".new-slider-2", {
     },
   },
 });
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 33500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
